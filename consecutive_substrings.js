f9153b08e8f88bb6874d171ba5b9cc474c8f5a16
Ian Synge <gamersynge@gmail.com>
2025-06-20 15:59:42 -0400

    Updated consecutive_substrings.js and solution.txt

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,28 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  //Initializing result array to return
+  const result = [];
+
+  //If string has a length of 0, return empty array
+  //If it has a length of 1, return an array with just the string
+  if (string.length === 0){
+    return result;
+  } else if (string.length === 1) {
+    result.push(string);
+    return result;
+  }
+
+  //Outer loop that will iterate along each substring in the string,
+  //to use each as a starting point
+  for (let i = 0; i < string.length; i++){
+    //Inner loop will iterate along each substring after the starting point,
+    //to use each as an ending point
+    for (let j = i + 1; j < string.length + 1; j++){
+      //Add the substring that goes from start point to end point to the result array
+      result.push(string.slice(i, j));
+    }
+  }
+
+  return result;
 }
 
 if (require.main === module) {
@@ -14,5 +37,3 @@ if (require.main === module) {
 }
 
 module.exports = consecutiveSubstrings;
-
-
